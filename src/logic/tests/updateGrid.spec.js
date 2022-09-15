814ed33afddfbf0e9bc1f2fc46d1522a0fdf48e2
Ruth Ng <ruth.ym.ng@gmail.com>
2022-09-15 17:35:01 +0100

    Add tests and logic to update the grid

---
+++ src/logic/tests/updateGrid.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import updateGrid from '../updateGrid.js';
+
+describe('updateGrid', () => {
+    it('is a function', () => {
+        expect(updateGrid).to.be.a('function');
+    });
+});
