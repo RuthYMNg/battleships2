9cd7660a4126230c2b26a4aed5f3809086061cfb
Ruth Ng <ruth.ym.ng@gmail.com>
2022-09-16 18:16:54 +0100

    Add default boats and boats object for use in React logic

---
+++ src/logic/boatsObject.js
@@ -0,0 +1,25 @@
+const boatsObject = {
+    carrier: {
+        name: "Carrier",
+        length: 5
+    },
+    battleship: {
+        name: "Battleship",
+        length: 4
+    },
+    cruiser: {
+        name: "Cruiser",
+        length: 3
+    },
+    submarine: {
+        name: "Submarine",
+        length: 3
+    },
+    destroyer: {
+        name: "Destroyer",
+        length: 2
+    },
+
+}
+
+export default boatsObject;
