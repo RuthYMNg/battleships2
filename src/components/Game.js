e10e9badd63942932a0333f4958624b51a5258e5
Ruth Ng <ruth.ym.ng@gmail.com>
2022-09-17 08:06:02 +0100

    Use and set initial state to set key aspects of the state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 
 import standardBoats from '../logic/standardBoats.js';
-import boatsObject from '../logic/boatsObject.js';
 import generateGrid from '../logic/generateGrid.js';
-import createGame from '../logic/createGame.js';
-import fire from '../logic/fire.js';
-import computerStrategy from '../logic/computerStrategy.js';
 
 const Game = () => {
+
+    const [ width, setWidth ] = useState(10);
+    const [ height, setHeight ] = useState(10);
+    const [ gridA, setGridA ] = useState(generateGrid());
+    const [ gridB, setGridB ] = useState(generateGrid());
+    const [ boats, setBoats ] = useState(standardBoats);
+    const [ numberOfBoats, setNumberOfBoats ] = useState(0);
+    const [ player, setPlayer ] = useState("A");
+    const [ computerStrategy, setComputerStrategy ] = useState({
+        next: [],
+        plan: [],
+        lastTry: []
+    });
+
   return (
     <div>
       <h3>Game</h3>
